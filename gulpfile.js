0a92ff59d68a3c00c9e9bf6f6d0c7eb5858cf922
hews <p@hews.co>
2015-10-14 00:18:05 -0700

    add css minify and set up watches; fix gulp piping

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,11 @@
-var gulp     = require('gulp-help')(require('gulp'));
-var scsslint = require('gulp-scss-lint');
-
-var changed  = require('gulp-changed');
-var rename   = require('gulp-rename');
-
-var sass     = require('gulp-sass');
-var svgo     = require('imagemin-svgo');
-
-// var concat     = require('gulp-concat');
-// var uglify     = require('gulp-uglify');
-// var sourcemaps = require('gulp-sourcemaps');
+var gulp       = require('gulp-help')(require('gulp'));
+var scsslint   = require('gulp-scss-lint');
+var changed    = require('gulp-changed');
+var rename     = require('gulp-rename');
+var sass       = require('gulp-sass');
+var svgo       = require('imagemin-svgo');
+var minifyCss  = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
 
 var es  = require('event-stream');
 var del = require('del');
@@ -31,27 +27,30 @@ gulp.task('clean', function() {
   return del([paths.dist, paths.examples]);
 });
 
-gulp.task('lint-scss', function() {
-  return gulp.src(paths.sourceScss)
-    .pipe(scsslint())
-    .pipe(scsslint.failReporter());
-});
-
 gulp.task('html', function () {
   return gulp.src(paths.sourceHtml)
     .pipe(rename('index.html'))
     .pipe(gulp.dest(paths.examples));
 });
 
-
-gulp.task('compile-scss', function () {
+gulp.task('lint-scss', function() {
+  return gulp.src(paths.sourceScss)
+    .pipe(scsslint())
+    .pipe(scsslint.failReporter());
+});
+gulp.task('compile-scss', ['lint-scss'], function () {
   return gulp.src(paths.sourceScss)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(paths.distCss))
+    .pipe(gulp.dest(paths.examplesCss))
+    .pipe(sourcemaps.init())
+      .pipe(minifyCss())
+    .pipe(sourcemaps.write())
+    .pipe(rename({extname: '.min.css'}))
+    .pipe(gulp.dest(paths.distCss))
     .pipe(gulp.dest(paths.examplesCss));
 });
-gulp.task('sass', ['lint-scss', 'compile-scss']);
-
+gulp.task('sass', ['compile-scss']);
 
 gulp.task('images', function() {
   var dist = gulp.src(paths.sourceSvgs)
@@ -67,34 +66,9 @@ gulp.task('images', function() {
   return es.concat(dist, examples);
 });
 
-gulp.task('example', ['sass', 'images', 'html']);
-
-// gulp.task('scripts', ['clean'], function() {
-//   // Minify and copy all JavaScript (except vendor scripts)
-//   // with sourcemaps all the way down
-//   return gulp.src(paths.scripts)
-//     .pipe(sourcemaps.init())
-//       .pipe(coffee())
-//       .pipe(uglify())
-//       .pipe(concat('all.min.js'))
-//     .pipe(sourcemaps.write())
-//     .pipe(gulp.dest('build/js'));
-// });
-
-// Copy all static images
-// gulp.task('images', ['clean'], function() {
-//   return gulp.src(paths.images)
-//     // Pass in options to the task
-//     .pipe(imagemin({optimizationLevel: 5}))
-//     .pipe(gulp.dest('build/img'));
-// });
-
-// Rerun the task when a file changes
-// gulp.task('watch', function() {
-//   gulp.watch(paths.scripts, ['scripts']);
-//   gulp.watch(paths.images, ['images']);
-// });
-
-gulp.task('default', ['help']);
-
-// gulp.task('watch')
+gulp.task('watch', function() {
+  gulp.watch(paths.sourceHtml, {verbose: true}, ['html']);
+  gulp.watch(paths.sourceScss, {verbose: true}, ['sass']);
+  gulp.watch(paths.sourceSvgs, {verbose: true}, ['images']);
+});
+gulp.task('default', ['watch']);
